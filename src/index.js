bbbbf92e4fc7fa325cfa1fdd4f10a2d8f8bc85eb
Yaniv Ben Zvi <yanivbenzvi023@gmail.com>
2021-03-05 11:03:10 +0200

    init project and configuration of ci cd process

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const port    = process.env.PORT || 4000
+const app     = express()
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+app.get('/profile', (req, res) => {
+    res.send('welcome to profile page')
+})
+
+app.listen(port, ()=>{
+    console.log(`server is up and running at: http://127.0.0.1:${port}`)
+})
